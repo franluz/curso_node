d1bd610758a0784d1d6d849ccad1660d414d70e5
Fran Luz <francielle.luz@gmail.com>
2019-04-08 17:00:34 -0300

    conclusao de modulo nodejs

--- app/router/admin.js
+++ app/router/admin.js
@@ -1,6 +1,10 @@
-module.exports=(app)=>{
-app.get('/formulario_inclusao_noticia',(req,res,next)=>{
-	res.render('admin/form_add_noticia');
-});
+module.exports = (app) => {
+	app.get('/formulario_inclusao_noticia', (req, res, next) => {
+		res.render('admin/form_add_noticia');
+	});
+	app.post('/noticias/salvar', (req, res, next) => {
+		var noticias=req.body;
+		res.send(noticias);
+	});
 }
 
--- config/server.js
+++ config/server.js
@@ -1,19 +1,15 @@
 var express = require('express');
 var consign = require('consign');
-
+var bodyparser = require('body-parser');
 var app = express();
 app.set('view engine','ejs');
 app.set('views','./app/views');
+app.use(bodyparser.urlencoded({extended:true}));
 
 consign()
     .include('app/router')
     .then('config/dbConnection.js')
     .then('app/models/noticiasModel.js')
     .into(app)
-    // consign({cwd: 'app'})
-    // .include('routes')
-    // .then('models')
-    // .then('controllers')
-    // .then('config/dbConnection.js')
-    // .into(app)
+ 
 module.exports = app;
